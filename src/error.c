bedd0f5b5a89a82ab071f321432b85c41c118b56
trmttty <tatsuya.tarumoto@gmail.com>
2020-11-11 02:40:26 +0900

    norm error.c

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: ttarumot <ttarumot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/27 23:04:48 by ttarumot          #+#    #+#             */
-/*   Updated: 2020/08/05 00:26:16 by ttarumot         ###   ########.fr       */
+/*   Updated: 2020/11/11 02:40:10 by ttarumot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	handle_error(int errnum, char *message, t_rt *rt)
 	ft_putstr_fd(" : ", 2);
 	ft_putstr_fd(message, 2);
 	ft_putstr_fd("\n", 2);
-	exit(1);	
+	exit(1);
 }
 
 void	handle_perror(char *message, t_rt *rt)
@@ -28,5 +28,5 @@ void	handle_perror(char *message, t_rt *rt)
 	(void)rt;
 	ft_putstr_fd("Error\n", 2);
 	perror(message);
-	exit(1);	
+	exit(1);
 }
