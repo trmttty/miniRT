0da8efa78ba4e85e253b4e0f4ec0e3a070540612
trmttty <tatsuya.tarumoto@gmail.com>
2020-11-12 10:34:22 +0900

    norm find_triangle.c

--- src/find_triangle.c
+++ src/find_triangle.c
@@ -6,11 +6,35 @@
 /*   By: ttarumot <ttarumot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 02:16:29 by ttarumot          #+#    #+#             */
-/*   Updated: 2020/11/11 02:19:16 by ttarumot         ###   ########.fr       */
+/*   Updated: 2020/11/12 10:31:31 by ttarumot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mini_rt.h>
+
+static int	find_intp(t_rt *rt, t_ray *ray, t_ip *intp, float t)
+{
+	t_tr		*tr;
+	t_vector	p;
+	t_vector	cp;
+
+	tr = rt->obj->shape.tr;
+	p = add(ray->start, multi(ray->dir, t));
+	if (dot(tr->normal, cross(sub(tr->v1, tr->v0), sub(p, tr->v0))) > 0
+	&& dot(tr->normal, cross(sub(tr->v2, tr->v1), sub(p, tr->v1))) > 0
+	&& dot(tr->normal, cross(sub(tr->v0, tr->v2), sub(p, tr->v2))) > 0)
+	{
+		intp->dist = t;
+		intp->pos = add(ray->start, multi(ray->dir, t));
+		if (dot(ray->dir, tr->normal) < 0)
+			intp->normal = tr->normal;
+		else
+			intp->normal = multi(tr->normal, -1);
+		normalize(&intp->normal);
+		return (1);
+	}
+	return (0);
+}
 
 int			find_triangle(t_rt *rt, t_ray *ray, t_ip *intp)
 {
@@ -26,24 +50,7 @@ int			find_triangle(t_rt *rt, t_ray *ray, t_ip *intp)
 		s_p = sub(ray->start, tr->v0);
 		t = -dot(s_p, tr->normal) / dn_dot;
 		if (t > 0)
-		{
-			t_vector p;
-
-			p = add(ray->start, multi(ray->dir, t));
-			if (dot(tr->normal, cross(sub(tr->v1, tr->v0), sub(p, tr->v0))) > 0
-			&& dot(tr->normal, cross(sub(tr->v2, tr->v1), sub(p, tr->v1))) > 0
-			&& dot(tr->normal, cross(sub(tr->v0, tr->v2), sub(p, tr->v2))) > 0)
-			{
-				intp->dist = t;
-				intp->pos = add(ray->start, multi(ray->dir, t));
-				if (dot(ray->dir, tr->normal) < 0)
-					intp->normal = tr->normal;
-				else
-					intp->normal = multi(tr->normal, -1);
-				normalize(&intp->normal);
-				return (1);
-			}
-		}
+			return (find_intp(rt, ray, intp, t));
 	}
 	return (0);
 }
